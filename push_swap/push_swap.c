bb7e96642797e3321c3e70be3c2802b4bc7527eb
Victor Moro <vmoroll98@gmail.com>
2025-06-19 11:53:22 +0200

    cambios main -> entrega push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vmoro-lu <vmoro-lu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:03:40 by vmoro-lu          #+#    #+#             */
-/*   Updated: 2025/06/19 11:50:13 by vmoro-lu         ###   ########.fr       */
+/*   Updated: 2025/06/19 11:53:12 by vmoro-lu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 	t_stack	b;
 
 	if (argc < 2)
-			return (0);
+		return (0);
 	init_stack(&a);
 	init_stack(&b);
 	if (!parse_stack(argv, &a))
